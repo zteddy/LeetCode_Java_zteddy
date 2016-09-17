5f42710a14a5018aeb4acbefb5442785dbc53847
zteddy <tz2307@columbia.edu>
2016-09-17 17:07:44 -0400

    AC original

---
+++ 1. Two Sum.java
@@ -0,0 +1,20 @@
+public class Solution {
+    public int[] twoSum(int[] nums, int target) {
+		
+		int i;
+		int j;
+		int[] result = new int[2];
+		for(i = 0; i < nums.length-1; i++){
+			for(j = i+1; j< nums.length; j++){
+				if(nums[i] + nums[j] == target){
+					result[0] = i;
+					result[1] = j;
+				}
+					
+			}
+		}
+		return result;
+        
+    }
+}
+//TODO none
