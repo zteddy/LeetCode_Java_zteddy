09567027d12575f1d38a592b52b5013e1f23d5f5
zteddy <tz2307@columbia.edu>
2016-09-17 17:08:25 -0400

    AC original

---
+++ 27. Remove Element.java
@@ -0,0 +1,26 @@
+public class Solution {
+    public int removeElement(int[] nums, int val) {
+		
+		int length = 0;
+		
+		int now = 0;
+		int search = 0;
+		
+		if(nums.length != 0){
+			
+			for(search = 0;search < nums.length; search++){
+			
+				if(nums[search] != val){
+					nums[now] = nums[search];
+					now++;
+					length++;
+				}
+				
+			}
+
+		}
+		
+		return length;
+        
+    }
+}
