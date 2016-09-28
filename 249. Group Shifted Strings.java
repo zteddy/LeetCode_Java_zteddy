766a4077ad1bebb94270aa202ea2f40e7ee82c6c
zteddy <tz2307@columbia.edu>
2016-09-27 23:32:08 -0400

    AC original

---
+++ 249. Group Shifted Strings.java
@@ -0,0 +1,42 @@
+public class Solution {
+    public List<List<String>> groupStrings(String[] strings) {
+        
+		List<List<String>> result = new ArrayList<List<String>>();
+		int flag = 0;
+		 
+		for(int i = 0; i < strings.length; i++){
+			flag = 0;
+			 for(int j = 0; j < result.size(); j++){
+				 String temp = result.get(j).get(0);
+				 if(temp.length() == strings[i].length() && flag == 0){
+					 
+					 int tlength = strings[i].length();
+					 int count = 0;
+					 for(int k = 0; k < tlength-1; k++){
+						 int a = temp.charAt(k+1) - temp.charAt(k);
+						 int b = strings[i].charAt(k+1) - strings[i].charAt(k);
+						 a = (a < 0)? a+26 : a;
+						 b = (b < 0)? b+26 : b;
+						 if(a == b) count++;
+						 
+					 }
+					 					 
+					 if(count == tlength-1){
+						 result.get(j).add(strings[i]); 
+						 flag = 1;  
+					 }
+				 }
+			 }
+			 if(flag == 0){
+				 List<String> nl = new ArrayList<String>();
+				 nl.add(strings[i]);
+				 result.add(nl);
+			 }
+		}
+		
+		return result;
+		
+    }
+}
+
+//TODO Using collection?
