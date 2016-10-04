d1ad4b526a8a8352c7e71e43d05b32e046c99c84
zteddy <tz2307@columbia.edu>
2016-10-04 01:22:18 -0400

    AC original & better

---
+++ 202. Happy Number.java
@@ -0,0 +1,64 @@
+public class Solution {
+	public int sosod(int n){
+		int digit = 0;
+		int remain = n;
+		int result = 0;
+
+		while(remain != 0){
+			digit = remain % 10;
+			remain = remain / 10;
+			result += digit * digit;
+		}
+
+		return result;
+	}
+
+    public boolean isHappy(int n) {
+
+        Map<Integer, Integer> hm = new HashMap<Integer, Integer>();
+
+        Integer s = sosod(n);
+        while(!hm.containsKey(s) && s != 1){
+        	hm.put(s,s);
+        	s = sosod(s);
+        }
+
+        if(s == 1)
+        	return true;
+        else
+        	return false;
+    }
+}
+
+/*Genius solution using math
+int digitSquareSum(int n) {
+    int sum = 0, tmp;
+    while (n) {
+        tmp = n % 10;
+        sum += tmp * tmp;
+        n /= 10;
+    }
+    return sum;
+}
+
+bool isHappy(int n) {
+    int slow, fast;
+    slow = fast = n;
+    do {
+        slow = digitSquareSum(slow);
+        fast = digitSquareSum(fast);
+        fast = digitSquareSum(fast);
+    } while(slow != fast);
+    if (slow == 1) return 1;
+    else return 0;
+}
+
+bool isHappy(int n) {
+    while(n>6){
+        int next = 0;
+        while(n){next+=(n%10)*(n%10); n/=10;}
+        n = next;
+    }
+    return n==1;
+}
+*/
