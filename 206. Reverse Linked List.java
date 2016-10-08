57585f8e9dd75f56844f56191ad4e35081fff0cd
zteddy <tz2307@columbia.edu>
2016-10-08 13:39:32 -0400

    AC original

---
+++ 206. Reverse Linked List.java
@@ -0,0 +1,32 @@
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode(int x) { val = x; }
+ * }
+ */
+public class Solution {
+    public ListNode reverseList(ListNode head) {
+
+        if(head == null || head.next == null) return head;
+
+        ListNode before = head;         //forget to put head.next to null;
+        ListNode now = head.next;
+        ListNode after;
+
+        while(now != null){
+        	after = now.next;
+        	now.next = before;
+        	before = now;
+        	now = after;
+        }
+
+        head.next = null;    //Important!
+
+        return before;
+
+    }
+}
+
+//TODO None
