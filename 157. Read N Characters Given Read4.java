4407cbc1b30515d8bcefb89d55ba6daf704fee58
zteddy <tz2307@columbia.edu>
2016-11-16 22:48:48 -0500

    WA original & AC better

--- 157. Read N Characters Given Read4.java
+++ 157. Read N Characters Given Read4.java
@@ -8,22 +8,47 @@ public class Solution extends Reader4 {
      * @return    The number of characters read
      */
     public int read(char[] buf, int n) {
-		
+
 		int count = 0;
-		count = read4(buf);
+		//count = read4(buf);
 		int temp = 0;
-		int result = 0;
-		
-		if(count >= n) return n;
-		
+		//int result = 0;
+
+		//if(count >= n) return n;
+
 		while(count < n){
 			temp = read4(buf);
 			count += temp;
-			if(temp < 4) return result = (count <= n)? count:n;
-			
+			//if(temp < 4) return result = (count <= n)? count:n;
+			if(temp == 0) break;
+
+
 		}
-		
-		return result = (count <= n)? count:n;
-        
+
+		return count;
+
     }
+}
+
+//Different from what I understand
+public int read(char[] buf, int n) {
+  boolean eof = false;      // end of file flag
+  int total = 0;            // total bytes have read
+  char[] tmp = new char[4]; // temp buffer
+
+  while (!eof && total < n) {
+    int count = read4(tmp);
+
+    // check if it's the end of the file
+    eof = count < 4;
+
+    // get the actual count
+    count = Math.min(count, n - total);
+
+    // copy from temp buffer to buf
+    for (int i = 0; i < count; i++)
+      buf[total++] = tmp[i];
+  }
+
+  return total;
 }
