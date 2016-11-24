60e6358ebecf5885c2dc2b46366a67b54d6498ba
zteddy <tz2307@columbia.edu>
2016-11-24 18:48:58 -0500

    WA original & AC better

---
+++ 2. Add Two Numbers.java
@@ -0,0 +1,74 @@
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode(int x) { val = x; }
+ * }
+ */
+public class Solution {
+	public int deCode(ListNode n){
+		int result = 0;
+		int k = 1;
+		while(n!=null){
+			result += n.val*k;
+			k *= 10;
+			n = n.next;
+		}
+		return result;
+	}
+
+	public ListNode enCode(int x){
+		ListNode head;
+        ListNode last;
+		ListNode temp;
+		last = new ListNode(x%10);
+		head = last;
+
+		while((x - x%10)/10 != 0){
+			x = (x - x%10)/10;
+			temp = new ListNode(x%10);
+			last.next = temp;
+			last = temp;
+		}
+
+		return head;
+	}
+
+    public ListNode addTwoNumbers(ListNode l1, ListNode l2) {
+    	int n1 = deCode(l1);
+    	int n2 = deCode(l2);
+
+    	return enCode(n1+n2);
+    }
+}
+
+//不能用这种方法，int会越界！！！
+
+//Add directly
+public class Solution {
+    public ListNode addTwoNumbers(ListNode l1, ListNode l2) {
+        ListNode c1 = l1;
+        ListNode c2 = l2;
+        ListNode sentinel = new ListNode(0);
+        ListNode d = sentinel;
+        int sum = 0;
+        while (c1 != null || c2 != null) {
+            sum /= 10;
+            if (c1 != null) {
+                sum += c1.val;
+                c1 = c1.next;
+            }
+            if (c2 != null) {
+                sum += c2.val;
+                c2 = c2.next;
+            }
+            d.next = new ListNode(sum % 10);
+            d = d.next;
+        }
+        if (sum / 10 == 1)
+            d.next = new ListNode(1);
+        return sentinel.next;
+    }
+}
+
