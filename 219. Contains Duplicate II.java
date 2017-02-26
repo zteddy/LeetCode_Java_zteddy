f9d3db30563695fb4aa18bc2c0ec43212bc654ba
zteddy <tz2307@columbia.edu>
2017-02-26 02:52:25 -0500

    Revised

--- 219. Contains Duplicate II.java
+++ 219. Contains Duplicate II.java
@@ -27,6 +27,8 @@ public boolean containsNearbyDuplicate(int[] nums, int k) {
         }
         return false;
  }
+ */
+
 
 
 /*Need new data structure
@@ -45,30 +47,3 @@ public class Solution {
     }
 }
 */
-
-/*Time Limit Exceeded
-public class Solution {
-    public boolean containsNearbyDuplicate(int[] nums, int k) {
-        for(int i = 1; i <= k; i++){
-			for(int j = 0; j+i < nums.length; j++){
-				if(nums[j] == nums[j+i]) return true;
-			}
-		}
-		return false;
-    }
-}
-*/
-
-/*Time Limit Exceeded
-public class Solution {
-    public boolean containsNearbyDuplicate(int[] nums, int k) {
-        for(int i = 0; i < nums.length; i++){
-			for(int j = 1; j <= k; j++){
-				if((i+j) < nums.length){
-					if(nums[i] == nums[i+j]) return true;
-				}
-			}
-		}
-		return false;
-    }
-}
