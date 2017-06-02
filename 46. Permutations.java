b1ee2c15bcf11fedb4f0374aed3bbc0b5757ed5c
zteddy <tz2307@columbia.edu>
2017-06-02 16:44:06 -0400

    AC original & better

---
+++ 46. Permutations.java
@@ -0,0 +1,249 @@
+public class Solution {
+    public void backTracking(int[] nums, boolean[] visit, List<Integer> temp, List<List<Integer>> result){
+        if(temp.size() == nums.length){
+            List<Integer> temptemp = new ArrayList<Integer>();
+            temptemp.addAll(temp);
+            result.add(temptemp);
+            temp = new ArrayList<Integer>();
+            return;
+        }
+        for(int i = 0; i < nums.length; i++){
+            if(!visit[i]){
+                visit[i] = true;
+                temp.add(nums[i]);
+                backTracking(nums, visit, temp, result);
+                temp.remove(temp.size()-1);
+                visit[i] = false;
+            }
+        }
+    }
+
+    public List<List<Integer>> permute(int[] nums) {
+        if(nums.length == 0) return null;
+
+        List<List<Integer>> result = new ArrayList<>();
+
+        boolean[] visit = new boolean[nums.length];
+
+        backTracking(nums, visit, new ArrayList<Integer>(), result);
+
+        return result;
+
+    }
+}
+
+
+
+/*66666
+This structure might apply to many other backtracking questions, but here I am just going to demonstrate Subsets, Permutations, and Combination Sum.
+
+Subsets : https://leetcode.com/problems/subsets/
+
+public List<List<Integer>> subsets(int[] nums) {
+    List<List<Integer>> list = new ArrayList<>();
+    Arrays.sort(nums);
+    backtrack(list, new ArrayList<>(), nums, 0);
+    return list;
+}
+
+private void backtrack(List<List<Integer>> list , List<Integer> tempList, int [] nums, int start){
+    list.add(new ArrayList<>(tempList));
+    for(int i = start; i < nums.length; i++){
+        tempList.add(nums[i]);
+        backtrack(list, tempList, nums, i + 1);
+        tempList.remove(tempList.size() - 1);
+    }
+}
+Subsets II (contains duplicates) : https://leetcode.com/problems/subsets-ii/
+
+public List<List<Integer>> subsetsWithDup(int[] nums) {
+    List<List<Integer>> list = new ArrayList<>();
+    Arrays.sort(nums);
+    backtrack(list, new ArrayList<>(), nums, 0);
+    return list;
+}
+
+private void backtrack(List<List<Integer>> list, List<Integer> tempList, int [] nums, int start){
+    list.add(new ArrayList<>(tempList));
+    for(int i = start; i < nums.length; i++){
+        if(i > start && nums[i] == nums[i-1]) continue; // skip duplicates
+        tempList.add(nums[i]);
+        backtrack(list, tempList, nums, i + 1);
+        tempList.remove(tempList.size() - 1);
+    }
+}
+Permutations : https://leetcode.com/problems/permutations/
+
+public List<List<Integer>> permute(int[] nums) {
+   List<List<Integer>> list = new ArrayList<>();
+   // Arrays.sort(nums); // not necessary
+   backtrack(list, new ArrayList<>(), nums);
+   return list;
+}
+
+private void backtrack(List<List<Integer>> list, List<Integer> tempList, int [] nums){
+   if(tempList.size() == nums.length){
+      list.add(new ArrayList<>(tempList));
+   } else{
+      for(int i = 0; i < nums.length; i++){
+         if(tempList.contains(nums[i])) continue; // element already exists, skip
+         tempList.add(nums[i]);
+         backtrack(list, tempList, nums);
+         tempList.remove(tempList.size() - 1);
+      }
+   }
+}
+Permutations II (contains duplicates) : https://leetcode.com/problems/permutations-ii/
+
+public List<List<Integer>> permuteUnique(int[] nums) {
+    List<List<Integer>> list = new ArrayList<>();
+    Arrays.sort(nums);
+    backtrack(list, new ArrayList<>(), nums, new boolean[nums.length]);
+    return list;
+}
+
+private void backtrack(List<List<Integer>> list, List<Integer> tempList, int [] nums, boolean [] used){
+    if(tempList.size() == nums.length){
+        list.add(new ArrayList<>(tempList));
+    } else{
+        for(int i = 0; i < nums.length; i++){
+            if(used[i] || i > 0 && nums[i] == nums[i-1] && !used[i - 1]) continue;
+            used[i] = true;
+            tempList.add(nums[i]);
+            backtrack(list, tempList, nums, used);
+            used[i] = false;
+            tempList.remove(tempList.size() - 1);
+        }
+    }
+}
+Combination Sum : https://leetcode.com/problems/combination-sum/
+
+public List<List<Integer>> combinationSum(int[] nums, int target) {
+    List<List<Integer>> list = new ArrayList<>();
+    Arrays.sort(nums);
+    backtrack(list, new ArrayList<>(), nums, target, 0);
+    return list;
+}
+
+private void backtrack(List<List<Integer>> list, List<Integer> tempList, int [] nums, int remain, int start){
+    if(remain < 0) return;
+    else if(remain == 0) list.add(new ArrayList<>(tempList));
+    else{
+        for(int i = start; i < nums.length; i++){
+            tempList.add(nums[i]);
+            backtrack(list, tempList, nums, remain - nums[i], i); // not i + 1 because we can reuse same elements
+            tempList.remove(tempList.size() - 1);
+        }
+    }
+}
+Combination Sum II (can't reuse same element) : https://leetcode.com/problems/combination-sum-ii/
+
+public List<List<Integer>> combinationSum2(int[] nums, int target) {
+    List<List<Integer>> list = new ArrayList<>();
+    Arrays.sort(nums);
+    backtrack(list, new ArrayList<>(), nums, target, 0);
+    return list;
+
+}
+
+private void backtrack(List<List<Integer>> list, List<Integer> tempList, int [] nums, int remain, int start){
+    if(remain < 0) return;
+    else if(remain == 0) list.add(new ArrayList<>(tempList));
+    else{
+        for(int i = start; i < nums.length; i++){
+            if(i > start && nums[i] == nums[i-1]) continue; // skip duplicates
+            tempList.add(nums[i]);
+            backtrack(list, tempList, nums, remain - nums[i], i + 1);
+            tempList.remove(tempList.size() - 1);
+        }
+    }
+}
+Palindrome Partitioning : https://leetcode.com/problems/palindrome-partitioning/
+
+public List<List<String>> partition(String s) {
+   List<List<String>> list = new ArrayList<>();
+   backtrack(list, new ArrayList<>(), s, 0);
+   return list;
+}
+
+public void backtrack(List<List<String>> list, List<String> tempList, String s, int start){
+   if(start == s.length())
+      list.add(new ArrayList<>(tempList));
+   else{
+      for(int i = start; i < s.length(); i++){
+         if(isPalindrome(s, start, i)){
+            tempList.add(s.substring(start, i + 1));
+            backtrack(list, tempList, s, i + 1);
+            tempList.remove(tempList.size() - 1);
+         }
+      }
+   }
+}
+
+public boolean isPalindrome(String s, int low, int high){
+   while(low < high)
+      if(s.charAt(low++) != s.charAt(high--)) return false;
+   return true;
+}
+*/
+
+
+
+
+/*using iterative
+the basic idea is, to permute n numbers, we can add the nth number into the resulting List<List<Integer>> from the n-1 numbers, in every possible position.
+
+For example, if the input num[] is {1,2,3}: First, add 1 into the initial List<List<Integer>> (let's call it "answer").
+
+Then, 2 can be added in front or after 1. So we have to copy the List<Integer> in answer (it's just {1}), add 2 in position 0 of {1}, then copy the original {1} again, and add 2 in position 1. Now we have an answer of {{2,1},{1,2}}. There are 2 lists in the current answer.
+
+Then we have to add 3. first copy {2,1} and {1,2}, add 3 in position 0; then copy {2,1} and {1,2}, and add 3 into position 1, then do the same thing for position 3. Finally we have 2*3=6 lists in answer, which is what we want.
+
+public List<List<Integer>> permute(int[] num) {
+    List<List<Integer>> ans = new ArrayList<List<Integer>>();
+    if (num.length ==0) return ans;
+    List<Integer> l0 = new ArrayList<Integer>();
+    l0.add(num[0]);
+    ans.add(l0);
+    for (int i = 1; i< num.length; ++i){
+        List<List<Integer>> new_ans = new ArrayList<List<Integer>>();
+        for (int j = 0; j<=i; ++j){
+           for (List<Integer> l : ans){
+               List<Integer> new_l = new ArrayList<Integer>(l);
+               new_l.add(j,num[i]);
+               new_ans.add(new_l);
+           }
+        }
+        ans = new_ans;
+    }
+    return ans;
+}
+*/
+
+
+
+/*Another backtracking
+public class Solution {
+    public List<List<Integer>> permute(int[] nums) {
+        List<List<Integer>> result = new ArrayList<List<Integer>>();
+        List<Integer> element = new ArrayList<Integer>();
+        generate(result, element, nums, 0);
+        return result;
+    }
+
+    private void generate(List<List<Integer>> result, List<Integer> element, int[] nums, int n){
+        if(n==nums.length){
+            result.add(element);
+            return;
+        }
+
+        int size = element.size();
+        for(int i = 0; i <= size; i++){
+            List<Integer> temp = new ArrayList<Integer>(element);
+            temp.add(i,nums[n]);
+            generate(result, temp, nums, n+1);
+        }
+        return;
+    }
+}
+*/
