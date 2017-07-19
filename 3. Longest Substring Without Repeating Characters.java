b73e49fad3df7bf15067b971686cbc6a714e398b
zteddy <tz2307@columbia.edu>
2017-07-18 20:48:02 -0400

    AC original & better

---
+++ 3. Longest Substring Without Repeating Characters.java
@@ -0,0 +1,182 @@
+public class Solution {
+    public int lengthOfLongestSubstring(String s) {
+        int maxLength = 0;
+        int[] pos = new int[256];
+        for(int i = 0; i<256; i++){
+            pos[i] = -1;
+        }
+        int head = 0;
+        int currentLength = 0;
+        for(int i = 0; i<s.length(); i++){
+            char now = s.charAt(i);
+
+            if(pos[now] == -1){
+                currentLength = i - head + 1;
+
+            }
+            else{
+                if(pos[now]+1 >= head)
+                    head = pos[now]+1;
+                currentLength = i - head + 1;
+            }
+            if(currentLength > maxLength){
+                maxLength = currentLength;
+            }
+            pos[now] = i;
+        }
+        return maxLength;
+    }
+}
+
+
+
+/*more concise but use hashmap
+the basic idea is, keep a hashmap which stores the characters in string as keys and their positions as values, and keep two pointers which define the max substring. move the right pointer to scan through the string , and meanwhile update the hashmap. If the character is already in the hashmap, then move the left pointer to the right of the same character last found. Note that the two pointers can only move forward.
+
+public int lengthOfLongestSubstring(String s) {
+    if (s.length()==0) return 0;
+    HashMap<Character, Integer> map = new HashMap<Character, Integer>();
+    int max=0;
+    for (int i=0, j=0; i<s.length(); ++i){
+        if (map.containsKey(s.charAt(i))){
+            j = Math.max(j,map.get(s.charAt(i))+1);
+        }
+        map.put(s.charAt(i),i);
+        max = Math.max(max,i-j+1);
+    }
+    return max;
+}
+*/
+
+
+
+/*using hashset
+public int lengthOfLongestSubstring(String s) {
+    int i = 0, j = 0, max = 0;
+    Set<Character> set = new HashSet<>();
+
+    while (j < s.length()) {
+        if (!set.contains(s.charAt(j))) {
+            set.add(s.charAt(j++));
+            max = Math.max(max, set.size());
+        } else {
+            set.remove(s.charAt(i++));
+        }
+    }
+
+    return max;
+}
+*/
+
+
+
+/*
+Approach #2 Sliding Window [Accepted]
+
+Algorithm
+
+The naive approach is very straightforward. But it is too slow. So how can we optimize it?
+
+In the naive approaches, we repeatedly check a substring to see if it has duplicate character. But it is unnecessary. If a substring s_{ij}s
+​ij
+​​  from index ii to j - 1j−1 is already checked to have no duplicate characters. We only need to check if s[j]s[j] is already in the substring s_{ij}s
+​ij
+​​ .
+
+To check if a character is already in the substring, we can scan the substring, which leads to an O(n^2)O(n
+​2
+​​ ) algorithm. But we can do better.
+
+By using HashSet as a sliding window, checking if a character in the current can be done in O(1)O(1).
+
+A sliding window is an abstract concept commonly used in array/string problems. A window is a range of elements in the array/string which usually defined by the start and end indices, i.e. [i, j)[i,j) (left-closed, right-open). A sliding window is a window "slides" its two boundaries to the certain direction. For example, if we slide [i, j)[i,j) to the right by 11 element, then it becomes [i+1, j+1)[i+1,j+1) (left-closed, right-open).
+
+Back to our problem. We use HashSet to store the characters in current window [i, j)[i,j) (j = ij=i initially). Then we slide the index jj to the right. If it is not in the HashSet, we slide jj further. Doing so until s[j] is already in the HashSet. At this point, we found the maximum size of substrings without duplicate characters start with index ii. If we do this for all ii, we get our answer.
+
+Java
+
+public class Solution {
+    public int lengthOfLongestSubstring(String s) {
+        int n = s.length();
+        Set<Character> set = new HashSet<>();
+        int ans = 0, i = 0, j = 0;
+        while (i < n && j < n) {
+            // try to extend the range [i, j]
+            if (!set.contains(s.charAt(j))){
+                set.add(s.charAt(j++));
+                ans = Math.max(ans, j - i);
+            }
+            else {
+                set.remove(s.charAt(i++));
+            }
+        }
+        return ans;
+    }
+}
+Complexity Analysis
+
+Time complexity : O(2n) = O(n)O(2n)=O(n). In the worst case each character will be visited twice by ii and jj.
+
+Space complexity : O(min(m, n))O(min(m,n)). Same as the previous approach. We need O(k)O(k) space for the sliding window, where kk is the size of the Set. The size of the Set is upper bounded by the size of the string nn and the size of the charset/alphabet mm.
+
+Approach #3 Sliding Window Optimized [Accepted]
+
+The above solution requires at most 2n steps. In fact, it could be optimized to require only n steps. Instead of using a set to tell if a character exists or not, we could define a mapping of the characters to its index. Then we can skip the characters immediately when we found a repeated character.
+
+The reason is that if s[j]s[j] have a duplicate in the range [i, j)[i,j) with index j'j
+​′
+​​ , we don't need to increase ii little by little. We can skip all the elements in the range [i, j'][i,j
+​′
+​​ ] and let ii to be j' + 1j
+​′
+​​ +1 directly.
+
+Java (Using HashMap)
+
+public class Solution {
+    public int lengthOfLongestSubstring(String s) {
+        int n = s.length(), ans = 0;
+        Map<Character, Integer> map = new HashMap<>(); // current index of character
+        // try to extend the range [i, j]
+        for (int j = 0, i = 0; j < n; j++) {
+            if (map.containsKey(s.charAt(j))) {
+                i = Math.max(map.get(s.charAt(j)), i);
+            }
+            ans = Math.max(ans, j - i + 1);
+            map.put(s.charAt(j), j + 1);
+        }
+        return ans;
+    }
+}
+Java (Assuming ASCII 128)
+
+The previous implements all have no assumption on the charset of the string s.
+
+If we know that the charset is rather small, we can replace the Map with an integer array as direct access table.
+
+Commonly used tables are:
+
+int[26] for Letters 'a' - 'z' or 'A' - 'Z'
+int[128] for ASCII
+int[256] for Extended ASCII
+public class Solution {
+    public int lengthOfLongestSubstring(String s) {
+        int n = s.length(), ans = 0;
+        int[] index = new int[128]; // current index of character
+        // try to extend the range [i, j]
+        for (int j = 0, i = 0; j < n; j++) {
+            i = Math.max(index[s.charAt(j)], i);
+            ans = Math.max(ans, j - i + 1);
+            index[s.charAt(j)] = j + 1;
+        }
+        return ans;
+    }
+}
+Complexity Analysis
+
+Time complexity : O(n)O(n). Index jj will iterate nn times.
+
+Space complexity (HashMap) : O(min(m, n))O(min(m,n)). Same as the previous approach.
+
+Space complexity (Table): O(m)O(m). mm is the size of the charset.
+*/
