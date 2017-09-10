a75a1bb98518966ea86e0feab8c91ebfa712d4d8
zteddy <tz2307@columbia.edu>
2017-09-10 01:12:50 -0400

    Revised

--- 76. Minimum Window Substring.java
+++ 76. Minimum Window Substring.java
@@ -144,49 +144,49 @@ string minWindow(string s, string t) {
         return d==INT_MAX? "":s.substr(head, d);
     }
 
-    Same code written in java
+Same code written in java
 
-    public String minWindow(String s, String t) {
-        HashMap<Character,Integer> map = new HashMap();
-        for(char c : s.toCharArray())
-            map.put(c,0);
-        for(char c : t.toCharArray())
-        {
-            if(map.containsKey(c))
-                map.put(c,map.get(c)+1);
-            else
-                return "";
-        }
-
-        int start =0, end=0, minStart=0,minLen = Integer.MAX_VALUE, counter = t.length();
-        while(end < s.length())
-        {
-            char c1 = s.charAt(end);
-            if(map.get(c1) > 0)
-                counter--;
-            map.put(c1,map.get(c1)-1);
-
-            end++;
-
-            while(counter == 0)
-            {
-                if(minLen > end-start)
-                {
-                    minLen = end-start;
-                    minStart = start;
-                }
-
-                char c2 = s.charAt(start);
-                map.put(c2, map.get(c2)+1);
-
-                if(map.get(c2) > 0)
-                    counter++;
-
-                start++;
-            }
-        }
-        return minLen == Integer.MAX_VALUE ? "" : s.substring(minStart,minStart+minLen);
+public String minWindow(String s, String t) {
+    HashMap<Character,Integer> map = new HashMap();
+    for(char c : s.toCharArray())
+        map.put(c,0);
+    for(char c : t.toCharArray())
+    {
+        if(map.containsKey(c))
+            map.put(c,map.get(c)+1);
+        else
+            return "";
     }
+
+    int start =0, end=0, minStart=0,minLen = Integer.MAX_VALUE, counter = t.length();
+    while(end < s.length())
+    {
+        char c1 = s.charAt(end);
+        if(map.get(c1) > 0)
+            counter--;
+        map.put(c1,map.get(c1)-1);
+
+        end++;
+
+        while(counter == 0)
+        {
+            if(minLen > end-start)
+            {
+                minLen = end-start;
+                minStart = start;
+            }
+
+            char c2 = s.charAt(start);
+            map.put(c2, map.get(c2)+1);
+
+            if(map.get(c2) > 0)
+                counter++;
+
+            start++;
+        }
+    }
+    return minLen == Integer.MAX_VALUE ? "" : s.substring(minStart,minStart+minLen);
+}
 
 
 Here comes the template.
