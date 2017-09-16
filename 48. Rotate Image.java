d4f137c580f6f0864f69ef198479d6e4695d66ba
zteddy <tz2307@columbia.edu>
2017-09-16 19:46:08 -0400

    AC original & better

---
+++ 48. Rotate Image.java
@@ -0,0 +1,44 @@
+class Solution {
+    public void rotate(int[][] matrix) {
+
+        int n = matrix[0].length;
+
+        //上下
+        for(int j = 0; j < n/2; j++){
+            for(int i = 0; i < (n-1)/2; i++){
+                int temp = matrix[i][j];
+                matrix[i][j] = matrix[n-1-i][j];
+                matrix[n-1-i][j] = temp;
+            }
+        }
+
+        //对角
+        for(int i = 0; i < n; i++){
+            for(int j = i; j < n; j++){
+                int temp = matrix[i][j];
+                matrix[i][j] = matrix[j][i];
+                matrix[j][i] = temp;
+            }
+        }
+
+    }
+}
+
+
+
+
+/*直接转？
+public class Solution {
+    public void rotate(int[][] matrix) {
+        int n=matrix.length;
+        for (int i=0; i<n/2; i++)
+            for (int j=i; j<n-i-1; j++) {
+                int tmp=matrix[i][j];
+                matrix[i][j]=matrix[n-j-1][i];
+                matrix[n-j-1][i]=matrix[n-i-1][n-j-1];
+                matrix[n-i-1][n-j-1]=matrix[j][n-i-1];
+                matrix[j][n-i-1]=tmp;
+            }
+        }
+}
+*/
